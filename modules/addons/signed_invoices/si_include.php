6f7ca864fec4977c6fcba65d56b48f47abfb20f6
thomas BOYER <thomas.boyer@interieur.gouv.fr>
2023-01-10 15:30:12 +0100

    test

--- modules/addons/signed_invoices/si_include.php
+++ modules/addons/signed_invoices/si_include.php
@@ -1,3 +1,12 @@
 <?php
 
 $status_signed_invoices = false;
+
+
+$dataFromModule = localAPI(
+    'GetModuleConfigurationParameters',
+    ['moduleType' => 'addon', 'moduleName' => 'signed_invoices']
+);
+
+var_dump($dataFromModule);
+exit();
